79cec479218cc965a1ab926c810abbfde5285265
UG-Skinner <c.skinner@blueyonder.co.uk>
2016-05-24 20:35:12 +0100

    Inital Commit

--- workshop5/exercise2/exercise2.js
+++ workshop5/exercise2/exercise2.js
@@ -1,10 +1,23 @@
 // The element to attach the listener to
 var buttonWrapper = document.getElementById('button-wrapper');
 
-
+/*
 buttonWrapper.addEventListener('click', function(event){
-	// console.log(event.target);
-	var attr = event.target.getAttribute('data-action');
-	console.log(attr);
 
+	console.log(event.target);
+	if(event.target.getAttribute('data-action')) {
+		var attr = event.target.getAttribute('data-action');
+		console.log(attr);
+	}
+	
+	
+
+});
+*/
+$( document ).ready(function() {
+    console.log( "ready!" );
+	
+	$('#button-wrapper').on('click', 'data-action', function(){
+		console.log('exists');
+	});
 });
