7675ed665229d3103106fbb36715db9cd5ad3a32
Chris Skinner <c.skinner@blueyonder.co.uk>
2016-06-01 23:17:22 +0100

    Recent re-structure

--- workshop6/exercise1/conf-ideas.js
+++ workshop6/exercise1/conf-ideas.js
@@ -2,31 +2,66 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
-	var previousFilter,
-		selection,
-		i,
-		boxes;
+	var i,
+		boxes,
+		row,
+		classNames = [],
+		reset,
+		results;
 
 	$('.list').on('click', 'input', function(event){
-		$(event.target).attr('data-filterMe', 'true');
-	
-		// Go get all the list inputs with data-filterMe="true"
-		// incase there have been previous clicks
-		selection = $('.list').attr('data-filterMe', 'true').attr('class');
 
+		// Set defualt
+		// reset = false;
+
+		// Get the title of the row to be used later
+		row = $(this).parents('.list').attr('data-attribute');
+		
 		// Hide all results
-		$('.boxes').removeClass('visible');
+		$('.results').find('.car').removeClass('active');
 
-		// Store the length
-		max = selection.length;
-		for(i=0; i < max; i++){
-			// For every element with a data-filterMe="true" remove classes based
-			// on the elements data-type
-			$('.boxes' + '.' + selection[i].attr("data-type")).addClass('visible');
-		}
-	
-		// Or
-		$('.boxes ' + selection).addClass('visible);
+		if( $(this).attr('value') == ('clear-' + row) ) {
+			console.log('reset button: ' + row);
+			//reset = true;
+
+		} else {
+			// Set all to FALSE
+			$(this).parents('.list[data-attribute="' + row + '"]').find('input').attr('data-filter', 'false');
+
+			// Set the current clicked to TRUE
+			$(this).attr('data-filter', 'true');
+
+		};
+
+		
+
+		// Go get all the list inputs with data-filter="true"
+		// incase there have been previous clicks
+		classNames = $(this).parents('.list-wrap').find('input[data-filter="true"]');
+
+		$.each(classNames, function(index, className) {
+			
+			className = $(className).attr('id');
+			console.log(className);
+			console.log(index);
+
+
+			// It would be nice to allow for a larger loop (if more filters are added in the future)
+			// Currently basing the following statement on an array size of 2
+			// Filtering down the already filterd results
+			// if(index !== classNames.length) didn't work
+			if(index === 0) {
+				
+				results = $('.results').find('.' + className);
+				results.addClass('active');
+			} else {
+				
+				results = $('.results').find(':not(.active.' + className + ')');
+				console.log(results);
+				results.removeClass('active');
+				console.log('index is not 1! See. ' + index);
+			}
+		});
 	});
 
 });
