6a1fafda75d5e4a441a86c665614a80443cc89b1
Chris Skinner <c.skinner@blueyonder.co.uk>
2016-06-02 00:01:04 +0100

    Recent re-structure

--- workshop6/exercise1/conf-ideas.js
+++ workshop6/exercise1/conf-ideas.js
@@ -3,10 +3,13 @@ $( document ).ready(function() {
 
 
 	var i,
+	    index,
 		boxes,
 		row,
 		classNames = [],
-		reset,
+		className,
+		resetNames = [],
+		resetName,
 		results;
 
 	$('.list').on('click', 'input', function(event){
@@ -20,24 +23,40 @@ $( document ).ready(function() {
 		// Hide all results
 		$('.results').find('.car').removeClass('active');
 
+		$(this).parents('.list[data-attribute="' + row + '"]').find('input').attr('data-filter', 'false');
+
 		if( $(this).attr('value') == ('clear-' + row) ) {
 			console.log('reset button: ' + row);
+
 			//reset = true;
+			resetNames = $('.list[data-attribute="' + row + '"]').find('input:not([id="clear-' + row + '"])');
+
+			console.log(resetNames);
+			$.each(resetNames, function(i, resetName) {
+				resetName = $(resetName).attr('id');
+				console.log(resetName);
+				$('.results').find('.' + resetName).removeClass('active');
+			});
 
 		} else {
-			// Set all to FALSE
-			$(this).parents('.list[data-attribute="' + row + '"]').find('input').attr('data-filter', 'false');
 
 			// Set the current clicked to TRUE
 			$(this).attr('data-filter', 'true');
 
 		};
 
-		
-
 		// Go get all the list inputs with data-filter="true"
 		// incase there have been previous clicks
 		classNames = $(this).parents('.list-wrap').find('input[data-filter="true"]');
+		console.log('length is: ' + classNames.length);
+
+		// If all data-filter="false" exit and reset results
+		// this means another filter name group has been set to 'clear' 
+		if(classNames.length === 0) {
+			$('.results').find('.car').addClass('active');
+			// exit
+			return;
+		} 
 
 		$.each(classNames, function(index, className) {
 			
@@ -51,17 +70,17 @@ $( document ).ready(function() {
 			// Filtering down the already filterd results
 			// if(index !== classNames.length) didn't work
 			if(index === 0) {
-				
+				console.log('its; 0');
 				results = $('.results').find('.' + className);
 				results.addClass('active');
 			} else {
-				
 				results = $('.results').find(':not(.active.' + className + ')');
 				console.log(results);
 				results.removeClass('active');
 				console.log('index is not 1! See. ' + index);
 			}
 		});
+
 	});
 
 });
