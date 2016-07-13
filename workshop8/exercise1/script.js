ac1f7dfe5d3850f2543d455e6c1dd03d6909240b
Chris Skinner <c.skinner@blueyonder.co.uk>
2016-07-13 17:02:32 +0100

    class edit updates

--- workshop8/exercise1/script.js
+++ workshop8/exercise1/script.js
@@ -20,26 +20,26 @@ var FormRecall = (function () {
     storeFieldValue = function (event) {
         // Get the "type" of node from event.target
         var typeOfnode = event.target.nodeName;
-        // Only act if type is "INPUT"
+        // Only act if type is "INPUT" (this would expand to include other form elements in a more complex form)
         if(typeOfnode === "INPUT") {
             // Get the value from field
             var itemValue = event.target.value;
-            //console.log('DEBUG: storeFieldValue: ' + itemValue + ' of ' + event.target.name);
 			window.localStorage.setItem(event.target.name, itemValue);
         }
     };
     
     populateForm = function (event) {
         event.preventDefault();
-        console.log('DEBUG: populateForm');
 
 		var inputs = form.querySelectorAll('input'),
 			max = inputs.length,
 			i,
 			currentInput;
 		
+        // Loop through form elements
 		for(i = 0; i < max; i++){
 			currentInput = inputs[i];
+             // Populate form with stored values if the current value exists
 			if( window.localStorage.getItem(currentInput.name) ) {
 				currentInput.setAttribute('value', window.localStorage.getItem(currentInput.name));
 			}
@@ -48,7 +48,6 @@ var FormRecall = (function () {
 
     clearStorage = function (event) {
         event.preventDefault();
-        console.log('DEBUG: clearStorage');
 		window.localStorage.clear();
     };
 
